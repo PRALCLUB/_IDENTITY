d00a9982254e1a2109b3191ded2cc0f125388e49
PRALCLUB <anais@pral.club>
2024-06-18 11:16:35 +0200

    Add files via upload

---
+++ IDENTITÉ/script.js
@@ -0,0 +1,220 @@
+// M = 1 WHAT WE BELIEVE : , 1-2 MARKET OVERVIEW, 1-3 CHALLENGE, 1-4 POSITIONNEMENT
+let main1 = document.querySelector(".main1");
+let m1 = document.querySelector(".m1");
+
+    let m1_1 = document.querySelector(".m1_1");
+        let m1_1_table = document.querySelector(".m1_1_table");
+    let m1_2 = document.querySelector(".m1_2");
+        let m1_2_table = document.querySelector(".m1_2_table");
+    let m1_3 = document.querySelector(".m1_3");
+        let m1_3_table = document.querySelector(".m1_3_table");
+    let m1_4 = document.querySelector(".m1_4");
+        let m1_4_table = document.querySelector(".m1_4_table");
+
+        main1.addEventListener("click", function(){
+            m1.classList.toggle("hide");
+            main1.classList.toggle("white");
+        })
+        m1_1.addEventListener("click", function(){
+            m1_1_table.classList.toggle("hide");
+            m1_1.classList.toggle("white");
+        })
+        m1_2.addEventListener("click", function(){
+            m1_2_table.classList.toggle("hide");
+            m1_2.classList.toggle("white");
+        })
+        m1_3.addEventListener("click", function(){
+            m1_3_table.classList.toggle("hide");
+            m1_3.classList.toggle("white");
+        })
+        m1_4.addEventListener("click", function(){
+            m1_4_table.classList.toggle("hide");
+            m1_4.classList.toggle("white");
+        })
+
+// M = 2 WHO WE ARE : 2-1 , 2-2 
+let main2 = document.querySelector(".main2");
+let m2 = document.querySelector(".m2");
+
+    let m2_1 = document.querySelector(".m2_1");
+        let m2_1_table = document.querySelector(".m2_1_table");
+    let m2_2 = document.querySelector(".m2_2");
+        let m2_2_table = document.querySelector(".m2_2_table");
+
+        main2.addEventListener("click", function(){
+            m2.classList.toggle("hide");
+            main2.classList.toggle("white");
+        })
+        m2_1.addEventListener("click", function(){
+            m2_1_table.classList.toggle("hide");
+            m2_1.classList.toggle("white");
+        })
+        m2_2.addEventListener("click", function(){
+            m2_2_table.classList.toggle("hide");
+            m2_2.classList.toggle("white");
+        })
+
+// M = 3 WHAT WE DO : 3-1 , 3-2 , 3-3 
+let main3 = document.querySelector(".main3");
+let m3 = document.querySelector(".m3");
+
+    let m3_1 = document.querySelector(".m3_1");
+        let m3_1_table = document.querySelector(".m3_1_table");
+    let m3_2 = document.querySelector(".m3_2");
+        let m3_2_table = document.querySelector(".m3_2_table");
+    let m3_3 = document.querySelector(".m3_3");
+        let m3_3_table = document.querySelector(".m3_3_table");
+
+        main3.addEventListener("click", function(){
+            m3.classList.toggle("hide");
+            main3.classList.toggle("white");
+        })
+        m3_1.addEventListener("click", function(){
+            m3_1_table.classList.toggle("hide");
+            m3_1.classList.toggle("white");
+        })
+        m3_2.addEventListener("click", function(){
+            m3_2_table.classList.toggle("hide");
+            m3_2.classList.toggle("white");
+        })
+        m3_3.addEventListener("click", function(){
+            m3_3_table.classList.toggle("hide");
+            m3_3.classList.toggle("white");
+        })
+
+// M = 4 WHAT WE DO : 4-1 , 4-2 , 4-3 , 4-4 , 4-5 ,
+let main4 = document.querySelector(".main4");
+let m4 = document.querySelector(".m4");
+
+    let m4_1 = document.querySelector(".m4_1");
+        let m4_1_table = document.querySelector(".m4_1_table");
+    let m4_2 = document.querySelector(".m4_2");
+        let m4_2_table = document.querySelector(".m4_2_table");
+    let m4_3 = document.querySelector(".m4_3");
+        let m4_3_table = document.querySelector(".m4_3_table");
+    let m4_4 = document.querySelector(".m4_4");
+        let m4_4_table = document.querySelector(".m4_4_table");
+
+        main4.addEventListener("click", function(){
+            m4.classList.toggle("hide");
+            main4.classList.toggle("white");
+        })
+        m4_1.addEventListener("click", function(){
+            m4_1_table.classList.toggle("hide");
+            m4_1.classList.toggle("white");
+        })
+        m4_2.addEventListener("click", function(){
+            m4_2_table.classList.toggle("hide");
+            m4_2.classList.toggle("white");
+        })
+        m4_3.addEventListener("click", function(){
+            m4_3_table.classList.toggle("hide");
+            m4_3.classList.toggle("white");
+        })
+        m4_4.addEventListener("click", function(){
+            m4_4_table.classList.toggle("hide");
+            m4_4.classList.toggle("white");
+        })
+
+let main5 = document.querySelector(".main5");
+let m5 = document.querySelector(".m5");
+
+    let m5_1 = document.querySelector(".m5_1");
+        let m5_1_table = document.querySelector(".m5_1_table");
+    let m5_2 = document.querySelector(".m5_2");
+        let m5_2_table = document.querySelector(".m5_2_table");
+    let m5_3 = document.querySelector(".m5_3");
+        let m5_3_table = document.querySelector(".m5_3_table");
+
+        main5.addEventListener("click", function(){
+            m5.classList.toggle("hide");
+            main5.classList.toggle("white");
+        })
+        m5_1.addEventListener("click", function(){
+            m5_1_table.classList.toggle("hide");
+            m5_1.classList.toggle("white");
+        })
+        m5_2.addEventListener("click", function(){
+            m5_2_table.classList.toggle("hide");
+            m5_2.classList.toggle("white");
+        })
+        m5_3.addEventListener("click", function(){
+            m5_3_table.classList.toggle("hide");
+            m5_3.classList.toggle("white");
+        }) 
+
+let main6 = document.querySelector(".main6");
+let m6 = document.querySelector(".m6");
+
+    let m6_1 = document.querySelector(".m6_1");
+        let m6_1_table = document.querySelector(".m6_1_table");
+    let m6_2 = document.querySelector(".m6_2");
+        let m6_2_table = document.querySelector(".m6_2_table");
+
+        main6.addEventListener("click", function(){
+            m6.classList.toggle("hide");
+            main6.classList.toggle("white");
+        })
+        m6_1.addEventListener("click", function(){
+            m6_1_table.classList.toggle("hide");
+            m6_1.classList.toggle("white");
+        })
+        m6_2.addEventListener("click", function(){
+            m6_2_table.classList.toggle("hide");
+            m6_2.classList.toggle("white");
+        })
+
+let main7 = document.querySelector(".main7");
+let m7 = document.querySelector(".m7");
+
+let m7_1 = document.querySelector(".m7_1");
+let m7_1_table = document.querySelector(".m7_1_table");
+let m7_2 = document.querySelector(".m7_2");
+let m7_2_table = document.querySelector(".m7_2_table");
+
+main7.addEventListener("click", function(){
+    m7.classList.toggle("hide");
+    main7.classList.toggle("white");
+})
+m7_1.addEventListener("click", function(){
+    m7_1_table.classList.toggle("hide");
+    m7_1.classList.toggle("white");
+})
+m7_2.addEventListener("click", function(){
+    m7_2_table.classList.toggle("hide");
+    m7_2.classList.toggle("white");
+})
+
+let main8 = document.querySelector(".main8");
+let m8 = document.querySelector(".m8");
+
+let main9 = document.querySelector(".main9");
+let m9 = document.querySelector(".m9");
+
+// let main10 = document.querySelector(".main10");
+// let m10 = document.querySelector(".m10");
+
+// let main11 = document.querySelector(".main11");
+// let m11 = document.querySelector(".m11");
+
+let main12 = document.querySelector(".main12");
+let m12 = document.querySelector(".m12");
+
+let main13 = document.querySelector(".main13");
+let m13 = document.querySelector(".m13");
+
+let main14 = document.querySelector(".main14");
+let m14 = document.querySelector(".m14");
+
+let main15 = document.querySelector(".main15");
+let m15 = document.querySelector(".m15");
+
+let main16 = document.querySelector(".main16");
+let m16 = document.querySelector(".m16");
+
+let main17 = document.querySelector(".main17");
+let m17 = document.querySelector(".m17");
+
+
+
+
